df516c584a26961eab0685a4fe00c575db4a8c8b
ay65535 <ay65535@icloud.com>
2018-04-19 08:21:57 +0900

    Add aliases

--- config/zsh/30_aliases.zsh
+++ config/zsh/30_aliases.zsh
@@ -17,4 +17,40 @@ alias eframe='emacsclient -a "" -c'
 alias ekill='emacsclient -e "(kill-emacs)"'
 
 alias q=exit
-alias ls='ls -F --color=auto'
+alias o=open
+
+case $OSTYPE in
+    'darwin'*)
+        DIRCOLORS_PATH='/usr/local/bin/gdircolors'
+        GNU_PREFIX='g'
+    ;;
+    'msys'*|*)
+        DIRCOLORS_PATH='/usr/bin/dircolors'
+        GNU_PREFIX=''
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x $DIRCOLORS_PATH ]; then
+    DIRCOLORS_NAME=${DIRCOLORS_PATH##*/}
+    test -r ~/.dircolors && eval "$($DIRCOLORS_NAME -b ~/.dircolors)" || eval "$($DIRCOLORS_NAME -b)"
+    alias ls="${GNU_PREFIX}ls --color=auto"
+    #alias ls='ls -G'
+    #alias dir="${GNU_PREFIX}dir --color=auto"
+    #alias vdir="${GNU_PREFIX}vdir --color=auto"
+
+    alias grep="${GNU_PREFIX}grep --color=auto"
+    alias fgrep="${GNU_PREFIX}fgrep --color=auto"
+    alias egrep="${GNU_PREFIX}egrep --color=auto"
+fi
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias l.='l -d .*'
+alias ll.='ll -d .*'
+alias md5='openssl md5'
+#alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
+type code-insiders >/dev/null 2>&1 && alias code=code-insiders
